f1e3985835f7b38df90a498dd7629e7bf7a46855
Dipto Barua <diptab11@gmail.com>
2024-08-15 01:51:16 +0900

    some error fixed regarding interfaces

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,19 +1,16 @@
-import { Heading, VStack, Box } from '@chakra-ui/react';
-import { type BaseComponent } from 'interfaces/component';
-import { useTranslation } from 'react-i18next';
-import { NavLink } from 'react-router-dom';
+import { Heading, VStack, Box } from "@chakra-ui/react";
+import { type BaseComponent } from "interfaces/component";
+import { NavLink } from "react-router-dom";
 
 const ErrorPage = (): BaseComponent => {
-  const { t } = useTranslation();
-
   return (
     <VStack spacing="10" p={10} align="center">
       <Heading fontSize="5xl" fontWeight="medium">
-        {t('error_page.heading1')}
+        ページが見つかりません。
       </Heading>
       <NavLink to="/">
-        <Box _hover={{ cursor: 'pointer', color: 'blue.400' }} color="blue.600">
-          {t('error_page.home_page')}{' '}
+        <Box _hover={{ cursor: "pointer", color: "blue.400" }} color="blue.600">
+          ホーム ページに戻ってください。
         </Box>
       </NavLink>
     </VStack>
